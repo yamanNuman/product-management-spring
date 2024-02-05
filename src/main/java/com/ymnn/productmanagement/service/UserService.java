1c9f35e597eca6f4d03c13365cdb84d742c6b1af
yamanNuman <yamannuman41@gmail.com>
2024-02-05 21:15:36 +0300

    jwtservice and userservice

---
+++ src/main/java/com/ymnn/productmanagement/service/UserService.java
@@ -0,0 +1,20 @@
+package com.ymnn.productmanagement.service;
+
+import com.ymnn.productmanagement.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+
+    //loadUserByUsername()
+    public UserDetailsService userDetailsService() {
+        return username -> userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}

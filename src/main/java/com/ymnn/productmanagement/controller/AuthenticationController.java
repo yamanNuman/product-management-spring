d2b31f3aa6617e489c5dd90b868694ab42a9a0c6
yamanNuman <yamannuman41@gmail.com>
2024-02-05 22:13:14 +0300

    security configuration

---
+++ src/main/java/com/ymnn/productmanagement/controller/AuthenticationController.java
@@ -0,0 +1,29 @@
+package com.ymnn.productmanagement.controller;
+
+import com.ymnn.productmanagement.dao.request.AuthenticationRequest;
+import com.ymnn.productmanagement.dao.request.RegisterRequest;
+import com.ymnn.productmanagement.dao.response.AuthenticationResponse;
+import com.ymnn.productmanagement.service.AuthenticationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+    private final AuthenticationService authenticationService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request) {
+        return ResponseEntity.ok(authenticationService.register(request));
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request) {
+        return ResponseEntity.ok(authenticationService.authenticate(request));
+    }
+}

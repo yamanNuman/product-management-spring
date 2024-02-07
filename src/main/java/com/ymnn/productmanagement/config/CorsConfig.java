9a9fc352abd0c4c1e616bfc6ff90b6d2a4064307
yamanNuman <yamannuman41@gmail.com>
2024-02-07 13:43:40 +0300

    cors config

---
+++ src/main/java/com/ymnn/productmanagement/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.ymnn.productmanagement.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebSecurity
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("*")
+                        .allowedOrigins("http://localhost:3000");
+            }
+        };
+    }
+}

cc4faca3f61752451806714478bf1fc3a0663fd1
yamanNuman <yamannuman41@gmail.com>
2024-02-08 17:05:06 +0300

    add authentication header

--- frontend/src/Components/Welcome/Welcome.jsx
+++ frontend/src/Components/Welcome/Welcome.jsx
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {jwtDecode} from "jwt-decode";
+import axios from "axios";
 
 const Welcome = () => {
+    const [products,setProducts] = useState(null);
+    async function fetchData() {
+        const result = await axios.get('http://localhost:8080/api/v1/products');
+        setProducts(result.data);
+    }
+    axios.interceptors.request.use(
+        config => {
+            config.headers.authorization = `Bearer ${userToken}`;
+            return config;
+        },
+        error => {
+            return Promise.reject(error);
+        }
+    )
     const userToken = localStorage.getItem('accessToken');
     const data = jwtDecode(userToken);
     const firstname = data.sub;
     return (
         <div>
             <h1>Welcome {firstname}</h1>
+            <div><button onClick={fetchData}>Call Spring</button></div>
+            <div>{JSON.stringify(products)}</div>
         </div>
     );
 };

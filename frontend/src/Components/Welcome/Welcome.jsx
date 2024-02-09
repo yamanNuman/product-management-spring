bfe1adfd10a8e83c504e194e5e62f2180bd2e05f
yamanNuman <yamannuman41@gmail.com>
2024-02-09 14:42:36 +0300

    get products

--- frontend/src/Components/Welcome/Welcome.jsx
+++ frontend/src/Components/Welcome/Welcome.jsx
@@ -1,15 +1,25 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {jwtDecode} from "jwt-decode";
 import axios from "axios";
+import 'bootstrap/dist/css/bootstrap.css';
 import ColorSchemesExample from "../Navbar/Navbar";
 
 const Welcome = () => {
 
-    const [products,setProducts] = useState(null);
-    async function fetchData() {
-        const result = await axios.get('http://localhost:8080/api/v1/products');
-        setProducts(result.data);
-    }
+    const [products,setProducts] = useState([]);
+
+    useEffect(() => {
+        axios
+            .get('http://localhost:8080/api/v1/products')
+            .then((response) => {
+                console.log(response.data);
+                setProducts(response.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    },[]);
+
     axios.interceptors.request.use(
         config => {
             config.headers.authorization = `Bearer ${userToken}`;
@@ -23,12 +33,38 @@ const Welcome = () => {
     const data = jwtDecode(userToken);
     const firstname = data.sub;
     return (
-        <div>
+       <div>
             <ColorSchemesExample></ColorSchemesExample>
+
             <h1>Welcome {firstname}</h1>
-            <div><button onClick={fetchData}>Call Spring</button></div>
-            <div>{JSON.stringify(products)}</div>
-        </div>
+                <h1>Products</h1>
+           <table className="table table-striped table-dark">
+                <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Product Name</th>
+                    <th>Supplier</th>
+                    <th>Stock</th>
+                    <th>Price</th>
+                    <th>Date</th>
+                </tr>
+                </thead>
+                <tbody>
+                {products.map((product,index) => {
+                    return <tr key={product.id}>
+                        <td>{index+1}</td>
+                        <td>{product.name}</td>
+                        <td>{product.supplier}</td>
+                        <td>{product.stock}</td>
+                        <td>{product.price[product.price.length-1].price}</td>
+                        <td>{product.price[product.price.length-1].date}</td>
+                    </tr>
+                })
+                }
+                </tbody>
+           </table>
+       </div>
+
     );
 };
 
